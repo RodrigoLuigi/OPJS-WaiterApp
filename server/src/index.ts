bb2d15b8866808f8c2f20237993f8551e59c9fd2
RodrigoLuigi <digaor@gmail.com>
2022-11-19 21:40:42 -0300

    feat: creates connection with mongodb and creates models Category/Order/Product

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import { router } from './router';
 
-const app = express();
-const port = 3000;
+mongoose
+  .connect('mongodb+srv://digaor:2717476@cluster0.e6vlilv.mongodb.net/test')
+  .then(() => {
+    const app = express();
+    const port = 3000;
 
-app.use(express.json());
-app.use(router);
+    app.use(express.json());
+    app.use(router);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port} 🚀`);
-});
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port} 🚀`);
+    });
+  })
+  .catch(() => console.log('error co conectar no mongodb'));
